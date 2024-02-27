113168629b22dfe17a7f9de03c3d36e0f04bec42
DevKumar00001 <96680248+DevKumar00001@users.noreply.github.com>
2024-02-27 12:07:13 +0530

    feat: Designed ProjectManagerActivity UI

--- app/src/main/java/com/tscodeeditor/blockly/android/activities/ProjectManagerActivity.java
+++ app/src/main/java/com/tscodeeditor/blockly/android/activities/ProjectManagerActivity.java
@@ -1,6 +1,9 @@
 package com.tscodeeditor.blockly.android.activities;
 
 import android.os.Bundle;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import com.tscodeeditor.blockly.android.R;
 import com.tscodeeditor.blockly.android.databinding.ActivityProjectManagerBinding;
 
 public class ProjectManagerActivity extends BaseActivity {
@@ -14,6 +17,24 @@ public class ProjectManagerActivity extends BaseActivity {
 
     // Set layout of activity
     setContentView(binding.getRoot());
+
+    binding.toolbar.setTitle(R.string.app_name);
+    setSupportActionBar(binding.toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setHomeButtonEnabled(true);
+    ActionBarDrawerToggle toggle =
+        new ActionBarDrawerToggle(
+            this, binding.drawerLayout, binding.toolbar, R.string.app_name, R.string.app_name);
+    binding.toolbar.setNavigationOnClickListener(
+        v -> {
+          if (binding.drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+          } else {
+            binding.drawerLayout.openDrawer(GravityCompat.START);
+          }
+        });
+    binding.drawerLayout.addDrawerListener(toggle);
+    toggle.syncState();
   }
 
   @Override
