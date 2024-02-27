ee1f86aff1c1b24527278a610f7d2c819b443d13
DevKumar00001 <96680248+DevKumar00001@users.noreply.github.com>
2024-02-27 09:12:26 +0530

    feat: Added BaseActivity(empty)

---
+++ app/src/main/java/com/tscodeeditor/blockly/android/activities/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.tscodeeditor.blockly.android.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+  /*
+   * BaseActivity contains configration for all activities
+   */
+  @Override
+  protected void onCreate(Bundle bundle) {
+    super.onCreate(bundle);
+  }
+}
