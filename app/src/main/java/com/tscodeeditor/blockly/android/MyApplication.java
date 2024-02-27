9fdf580a18a01d83dd4314916c9d9e1176c162a8
DevKumar00001 <96680248+DevKumar00001@users.noreply.github.com>
2024-02-27 09:15:10 +0530

    feat(license): Added license text in files

--- app/src/main/java/com/tscodeeditor/blockly/android/MyApplication.java
+++ app/src/main/java/com/tscodeeditor/blockly/android/MyApplication.java
@@ -1,3 +1,20 @@
+/*
+ *  This file is part of Blockly Android.
+ *
+ *  Blockly Android is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Blockly Android is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with Blockly Android.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.tscodeeditor.blockly.android;
 
 import android.app.Application;
