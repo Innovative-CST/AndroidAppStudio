87174c7b957546c28049c2cf161c5b7236c4c041
DevKumar00001 <96680248+DevKumar00001@users.noreply.github.com>
2024-02-28 09:55:16 +0530

    CHANGE APP NAME AND PACKAGE NAME

--- app/src/main/java/com/tscodeeditor/android/appstudio/MyApplication.java
+++ app/src/main/java/com/tscodeeditor/android/appstudio/MyApplication.java
@@ -1,21 +1,21 @@
 /*
- *  This file is part of Blockly Android.
+ *  This file is part of Android AppStudio.
  *
- *  Blockly Android is free software: you can redistribute it and/or modify
+ *  Android AppStudio is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Blockly Android is distributed in the hope that it will be useful,
+ *  Android AppStudio is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *   along with Blockly Android.  If not, see <https://www.gnu.org/licenses/>.
+ *   along with Android AppStudio.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.tscodeeditor.blockly.android;
+package com.tscodeeditor.android.appstudio;
 
 import android.app.Application;
 import android.content.Context;
--- app/src/main/java/com/tscodeeditor/android/appstudio/activities/ProjectManagerActivity.java
+++ app/src/main/java/com/tscodeeditor/android/appstudio/activities/ProjectManagerActivity.java
@@ -1,27 +1,27 @@
 /*
- *  This file is part of Blockly Android.
+ *  This file is part of Android AppStudio.
  *
- *  Blockly Android is free software: you can redistribute it and/or modify
+ *  Android AppStudio is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Blockly Android is distributed in the hope that it will be useful,
+ *  Android AppStudio is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *   along with Blockly Android.  If not, see <https://www.gnu.org/licenses/>.
+ *   along with Android AppStudio.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.tscodeeditor.blockly.android.activities;
+package com.tscodeeditor.android.appstudio.activities;
 
 import android.os.Bundle;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
-import com.tscodeeditor.blockly.android.R;
-import com.tscodeeditor.blockly.android.databinding.ActivityProjectManagerBinding;
+import com.tscodeeditor.android.appstudio.R;
+import com.tscodeeditor.android.appstudio.databinding.ActivityProjectManagerBinding;
 
 public class ProjectManagerActivity extends BaseActivity {
   private ActivityProjectManagerBinding binding;
