6d7f48fc397cff6e5f1e83cb3e04c597dba6e6b3
DevKumar00001 <96680248+DevKumar00001@users.noreply.github.com>
2024-03-14 15:45:56 +0530

    feat(Event): Added name field in Event model

--- block/src/main/java/com/tscodeeditor/android/appstudio/block/model/Event.java
+++ block/src/main/java/com/tscodeeditor/android/appstudio/block/model/Event.java
@@ -3,10 +3,20 @@ package com.tscodeeditor.android.appstudio.block.model;
 import java.io.Serializable;
 
 public class Event implements Serializable, Cloneable {
+  private String name;
 
   @Override
   public Event clone() throws CloneNotSupportedException {
     Event event = new Event();
+    event.setName(new String(getName()));
     return event;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 }
