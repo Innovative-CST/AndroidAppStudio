eb888bd7188e3ab8741b0b7eba0ca3b67b1272f6
DevKumar00001 <96680248+DevKumar00001@users.noreply.github.com>
2024-03-20 19:57:27 +0530

    feat(model): add BlockModel

---
+++ block/src/main/java/com/tscodeeditor/android/appstudio/block/model/BlockModel.java
@@ -0,0 +1,123 @@
+/*
+ * This file is part of Android AppStudio [https://github.com/TS-Code-Editor/AndroidAppStudio].
+ *
+ * License Agreement
+ * This software is licensed under the terms and conditions outlined below. By accessing, copying, modifying, or using this software in any way, you agree to abide by these terms.
+ *
+ * 1. **  Copy and Modification Restrictions  **
+ *    - You are not permitted to copy or modify the source code of this software without the permission of the owner, which may be granted publicly on GitHub Discussions or on Discord.
+ *    - If permission is granted by the owner, you may copy the software under the terms specified in this license agreement.
+ *    - You are not allowed to permit others to copy the source code that you were allowed to copy by the owner.
+ *    - Modified or copied code must not be further copied.
+ * 2. **  Contributor Attribution  **
+ *    - You must attribute the contributors by creating a visible list within the application, showing who originally wrote the source code.
+ *    - If you copy or modify this software under owner permission, you must provide links to the profiles of all contributors who contributed to this software.
+ * 3. **  Modification Documentation  **
+ *    - All modifications made to the software must be documented and listed.
+ *    - the owner may incorporate the modifications made by you to enhance this software.
+ * 4. **  Consistent Licensing  **
+ *    - All copied or modified files must contain the same license text at the top of the files.
+ * 5. **  Permission Reversal  **
+ *    - If you are granted permission by the owner to copy this software, it can be revoked by the owner at any time. You will be notified at least one week in advance of any such reversal.
+ *    - In case of Permission Reversal, if you fail to acknowledge the notification sent by us, it will not be our responsibility.
+ * 6. **  License Updates  **
+ *    - The license may be updated at any time. Users are required to accept and comply with any changes to the license.
+ *    - In such circumstances, you will be given 7 days to ensure that your software complies with the updated license.
+ *    - We will not notify you about license changes; you need to monitor the GitHub repository yourself (You can enable notifications or watch the repository to stay informed about such changes).
+ * By using this software, you acknowledge and agree to the terms and conditions outlined in this license agreement. If you do not agree with these terms, you are not permitted to use, copy, modify, or distribute this software.
+ *
+ * Copyright © 2024 Dev Kumar
+ */
+
+package com.tscodeeditor.android.appstudio.block.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class BlockModel implements Serializable, Cloneable {
+  private static final long serialVersionUID = 021173507L;
+
+  private String color;
+  private String rawCode;
+  private String[] returns;
+  private String[] tags;
+  private ArrayList<ArrayList<BlockContentModel>> blockContentModel;
+
+  public ArrayList<ArrayList<BlockContentModel>> getBlockContentModel() {
+    return this.blockContentModel;
+  }
+
+  public void setBlockContentModel(ArrayList<ArrayList<BlockContentModel>> blockContentModel) {
+    this.blockContentModel = blockContentModel;
+  }
+
+  public String getColor() {
+    return this.color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public String getRawCode() {
+    return this.rawCode;
+  }
+
+  public void setRawCode(String rawCode) {
+    this.rawCode = rawCode;
+  }
+
+  public String[] getReturns() {
+    return this.returns;
+  }
+
+  public void setReturns(String[] returns) {
+    this.returns = returns;
+  }
+
+  public String[] getTags() {
+    return this.tags;
+  }
+
+  public void setTags(String[] tags) {
+    this.tags = tags;
+  }
+
+  @Override
+  protected BlockModel clone() {
+    BlockModel block = new BlockModel();
+    block.setColor(new String(getColor() == null ? "" : getColor()));
+    block.setRawCode(new String(getRawCode() == null ? "" : getRawCode()));
+
+    String[] clonedTags = new String[] {};
+    for (int position = 0; position < getTags().length; ++position) {
+      clonedTags[position] = new String(getTags()[position] == null ? "" : getTags()[position]);
+    }
+
+    String[] clonedReturns = new String[] {};
+    for (int position = 0; position < getReturns().length; ++position) {
+      clonedReturns[position] =
+          new String(getReturns()[position] == null ? "" : getReturns()[position]);
+    }
+
+    ArrayList<ArrayList<BlockContentModel>> blockContentModel =
+        new ArrayList<ArrayList<BlockContentModel>>();
+
+    /*
+     * Complex algorithm to deep clone the ArrayList of ArrayList of BlockContentModel.
+     */
+    for (int layers = 0; layers < getBlockContentModel().size(); ++layers) {
+      ArrayList<BlockContentModel> layer = new ArrayList<BlockContentModel>();
+      for (int blockContentCount = 0;
+          blockContentCount < getBlockContentModel().get(layers).size();
+          ++blockContentCount) {
+        BlockContentModel clonedBlockContentModel =
+            getBlockContentModel().get(layers).get(blockContentCount).clone();
+        layer.add(clonedBlockContentModel);
+      }
+      blockContentModel.add(layer);
+    }
+
+    return block;
+  }
+}
