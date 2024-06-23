0a193e8f0d9579ae423b02404b73de52f5bf2c14
DevKumar00001 <96680248+DevKumar00001@users.noreply.github.com>
2024-06-23 18:48:10 +0530

    feat: add theme teal

--- themeEngine/src/main/java/com/quickersilver/themeengine/Theme.java
+++ themeEngine/src/main/java/com/quickersilver/themeengine/Theme.java
@@ -5,7 +5,8 @@ import androidx.annotation.StyleRes;
 
 public enum Theme {
   AMBER(R.style.Theme_ThemeEngine_Amber, R.color.amber_theme_primary),
-  BLUE(R.style.Theme_ThemeEngine_Blue, R.color.blue_theme_primary);
+  BLUE(R.style.Theme_ThemeEngine_Blue, R.color.blue_theme_primary),
+  TEAL(R.style.Theme_ThemeEngine_Teal, R.color.teal_theme_primary);
 
   @StyleRes private final int themeId;
 
