bcbde3b69b1acc10cd30c1dc9f7aa6aecc261d97
DevKumar00001 <96680248+DevKumar00001@users.noreply.github.com>
2024-09-05 14:37:31 +0530

    feat: add layout chooser

--- app/src/main/java/com/icst/android/appstudio/bottomsheet/EditLayoutVariableModelBottomSheet.java
+++ app/src/main/java/com/icst/android/appstudio/bottomsheet/EditLayoutVariableModelBottomSheet.java
@@ -33,15 +33,26 @@ package com.icst.android.appstudio.bottomsheet;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.icst.android.appstudio.R;
+import com.icst.android.appstudio.block.model.FileModel;
 import com.icst.android.appstudio.databinding.BottomsheetLayoutVariableModelEditorBinding;
 import com.icst.android.appstudio.listener.LayoutVariableModelChangeListener;
 import com.icst.android.appstudio.models.ModuleModel;
+import com.icst.android.appstudio.utils.EnvironmentUtils;
+import com.icst.android.appstudio.utils.FileModelUtils;
+import com.icst.android.appstudio.utils.serialization.DeserializerUtils;
+import com.icst.android.appstudio.vieweditor.models.LayoutModel;
 import com.icst.android.appstudio.vieweditor.models.LayoutVariableModel;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class EditLayoutVariableModelBottomSheet extends BottomSheetDialog {
 
   private LayoutVariableModel model;
+  private ModuleModel module;
 
   public EditLayoutVariableModelBottomSheet(
       Context context,
@@ -50,11 +61,20 @@ public class EditLayoutVariableModelBottomSheet extends BottomSheetDialog {
       LayoutVariableModelChangeListener listener) {
     super(context);
     this.model = model;
+    this.module = module;
 
     BottomsheetLayoutVariableModelEditorBinding binding =
         BottomsheetLayoutVariableModelEditorBinding.inflate(LayoutInflater.from(context));
 
     setContentView(binding.getRoot());
+
+    ArrayAdapter adapter =
+        new ArrayAdapter<String>(
+            getContext(), R.layout.autocomplete_adapter_layout_chooser, R.id.variableName);
+    ArrayList<String> layoutNames = new ArrayList<String>();
+    addLayoutNamesToList(layoutNames, adapter);
+
+    binding.layoutName.setAdapter(adapter);
 
     if (this.model == null) {
       binding.delete.setText("Cancel");
@@ -77,5 +97,50 @@ public class EditLayoutVariableModelBottomSheet extends BottomSheetDialog {
           listener.onLayoutVariableModelUpdate(this.model);
           dismiss();
         });
+  }
+
+  public void addLayoutNamesToList(ArrayList<String> layoutNames, ArrayAdapter<String> adapter) {
+    File resListDirectory = new File(module.resourceDirectory, EnvironmentUtils.FILES);
+    ArrayList<FileModel> resFolders = FileModelUtils.getFileModelList(resListDirectory);
+
+    if (resFolders == null) return;
+
+    for (int position = 0; position < resFolders.size(); ++position) {
+
+      if (Pattern.compile("^layout(?:-[a-zA-Z0-9]+)?$")
+          .matcher(resFolders.get(position).getName())
+          .matches()) {
+
+        File layoutsDir =
+            new File(
+                resListDirectory,
+                resFolders
+                    .get(position)
+                    .getName()
+                    .concat(File.separator)
+                    .concat(EnvironmentUtils.FILES));
+
+        if (!layoutsDir.exists()) return;
+
+        for (File layoutFile : layoutsDir.listFiles()) {
+          LayoutModel layoutFileModel =
+              DeserializerUtils.deserialize(layoutFile, LayoutModel.class);
+
+          if (layoutFileModel != null) {
+            boolean alreadyPresentLayout = false;
+            for (int i = 0; i < layoutNames.size(); ++i) {
+              if (layoutNames.get(i).equals(layoutFileModel.getLayoutName())) {
+                alreadyPresentLayout = true;
+              }
+            }
+
+            if (!alreadyPresentLayout) {
+              layoutNames.add(layoutFileModel.getLayoutName());
+              adapter.add(layoutFileModel.getLayoutName());
+            }
+          }
+        }
+      }
+    }
   }
 }
